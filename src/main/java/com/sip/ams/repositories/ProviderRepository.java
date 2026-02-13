2b3922ee0c9974c3e9e5b92feaeab159660a915b
MariamCHARRADA <charradamariam@yahoo.com>
2026-02-13 13:22:21 +0100

    add annotation

--- src/main/java/com/sip/ams/repositories/ProviderRepository.java
+++ src/main/java/com/sip/ams/repositories/ProviderRepository.java
@@ -1,5 +1,6 @@
 package com.sip.ams.repositories;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sip.ams.entities.Provider;
 
