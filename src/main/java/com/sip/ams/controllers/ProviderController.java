18c280ff99d3046314edbeedd2a66bf829ff6746
MariamCHARRADA <charradamariam@yahoo.com>
2026-02-16 17:25:40 +0100

    implement getProviderById

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -1,12 +1,13 @@
 package com.sip.ams.controllers;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity; //What really changes API return status
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,27 +25,36 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 public class ProviderController {
 	@Autowired
 	ProviderRepository providerRepository;
-	
+
 	@GetMapping("/")
-	@Operation(summary = "Fetching all providers") //Mere annotations for Swagger
-	@ApiResponses(value = {
-	    @ApiResponse(responseCode = "200", description = "getAllProviders Successs"),
-	    @ApiResponse(responseCode = "500", description = "Problem fetching providers")
-	})
-	public ResponseEntity<List<Provider>> getAllProviders() { 
-		return new ResponseEntity<> ((List<Provider>)providerRepository.findAll(), HttpStatus.OK);
+	@Operation(summary = "Fetching all providers") // Mere annotations for Swagger
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "getAllProviders Successs"),
+			@ApiResponse(responseCode = "500", description = "Problem fetching providers") })
+	public ResponseEntity<List<Provider>> getAllProviders() {
+		return new ResponseEntity<>((List<Provider>) providerRepository.findAll(), HttpStatus.OK);
 	}
-	
+
+	@GetMapping("/{id}") //id id is a variable
+	@Operation(summary = "Fetching provider by ID")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Provider is found"),
+			@ApiResponse(responseCode = "404", description = "Provider not found") })
+	public ResponseEntity<Provider> getProviderById(@PathVariable int id) { //id is retrieved from the path URL
+
+		Optional<Provider> opt = this.providerRepository.findById(id);
+
+		if (opt.isEmpty())
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		else
+			return new ResponseEntity<>(opt.get(), HttpStatus.OK);
+	}
+
 	@PostMapping("/")
 	@Operation(summary = "Adding new providers")
-	@ApiResponses(value = { 
-	    @ApiResponse(responseCode = "201", description = "new provider created"),
-	    @ApiResponse(responseCode = "500", description = "Problem adding provider")
-	})
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "new provider created"),
+			@ApiResponse(responseCode = "500", description = "Problem adding provider") })
 	public ResponseEntity<Provider> addProvider(@RequestBody Provider p) {
 
 		return new ResponseEntity<>(providerRepository.save(p), HttpStatus.CREATED);
 	}
-	
 
 }
